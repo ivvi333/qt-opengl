81769d92a98295caecd8ce332cbf527fd750e167
shenyanyanya <vanya.toryanik@yandex.ru>
2022-12-20 14:52:22 +0300

    Random tree generation!

--- glwindow.cpp
+++ glwindow.cpp
@@ -1,5 +1,7 @@
 #include "glwindow.h"
 
+#include <random>
+
 void getQuadVertices(QVector3D *vertices, float width, float length, float height, float x, float y, float z)
 {
     float hw = width / 2, hl = length / 2, hh = height / 2;
@@ -41,18 +43,84 @@ void getQuadVertices(QVector3D *vertices, float width, float length, float heigh
     vertices[23] = { x - hw, y - hl, z + hh };
 }
 
-GLWindow::GLWindow()
+Tree::Tree(int type, float width, float length, float height, float x, float y, float z)
+    : type(type)
 {
+    treeTruncColor = QVector3D(74.0/255.0, 53.0/255.0, 42.0/255.0);
+
+    treeTrunc = std::make_unique<QVector3D[]>(24);
+    getQuadVertices(treeTrunc.get(), width / 3, length / 3, height - height / 12, x, y, z - height / 24);
+
+    if (width > length)
+        length = width;
+    else
+        width = length;
+
+    switch (type) {
+    case 1:
+        treeLeavesColor = QVector3D(220.0/255.0, 225.0/255.0, 120.0/255.0);
+        treeLeaves.push_back(std::make_unique<QVector3D[]>(24));
+        getQuadVertices(treeLeaves[treeLeaves.size() - 1].get(), width, length, height / 2, x, y, z + height / 4);
+        break;
+
+    case 2:
+        treeLeavesColor = QVector3D(40.0/255.0, 80.0/255.0, 25.0/255.0);
+        treeLeaves.push_back(std::make_unique<QVector3D[]>(24));
+        getQuadVertices(treeLeaves[treeLeaves.size() - 1].get(), width, length, 3 * height / 4, x, y, z + height / 8);
+        break;
+
+    case 3:
+        treeLeavesColor = QVector3D(20.0/255.0, 35.0/255.0, 20.0/255.0);
+        treeLeaves.push_back(std::make_unique<QVector3D[]>(24));
+        getQuadVertices(treeLeaves[treeLeaves.size() - 1].get(), width, length, height / 6, x, y, z + 5 * height / 12);
+        treeLeaves.push_back(std::make_unique<QVector3D[]>(24));
+        getQuadVertices(treeLeaves[treeLeaves.size() - 1].get(), width, length, height / 6, x, y, z + 2 * height / 12);
+        treeLeaves.push_back(std::make_unique<QVector3D[]>(24));
+        getQuadVertices(treeLeaves[treeLeaves.size() - 1].get(), width, length, height / 6, x, y, z - height / 12);
+        treeLeaves.push_back(std::make_unique<QVector3D[]>(24));
+        getQuadVertices(treeLeaves[treeLeaves.size() - 1].get(), width, length, height / 6, x, y, z - 4 * height / 12);
+        break;
+    }
 }
 
-GLWindow::~GLWindow()
-{
+void GLWindow::addTree() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    std::uniform_int_distribution<int> cellDis(0, freeCells.size() - 1);
+    int cellIndex = cellDis(gen);
+
+    std::uniform_int_distribution<int> typeDis(1, 3);
+    int type = typeDis(gen);
+
+    std::uniform_real_distribution<float> widthDis(10.0, 45.0);
+    float width = widthDis(gen);
+
+    std::uniform_real_distribution<float> lengthDis(10.0, 45.0);
+    float length = lengthDis(gen);
+
+    std::uniform_real_distribution<float> heightDis(20.0, 70.0);
+    float height = heightDis(gen);
+
+    trees.push_back(Tree(type, width, length, height,
+                         cellW / 2 + cellW * freeCells[cellIndex].first - platformW / 2,
+                         cellL / 2 + cellL * freeCells[cellIndex].second - platformL / 2,
+                         platformH / 2 + height / 2));
+    freeCells.erase(freeCells.begin() + cellIndex);
+}
+
+void GLWindow::freeAllCells() {
+    freeCells.clear();
+    for (int i = 0; i < platformW / cellW; i++)
+        for (int j = 0; j < platformL / cellL; j++)
+            freeCells.push_back(std::pair<int, int>(i, j));
 }
 
 void GLWindow::resetProjection()
 {
     projectMatrix.setToIdentity();
-    projectMatrix.perspective(30.0, (float)width()/height(), 0.1, 100);
+    projectMatrix.perspective(30.0, (float)width()/height(), 0.1,
+                              (platformL > platformW ? platformL * 2 : platformW * 2));
 }
 
 void GLWindow::resetModelView()
@@ -69,20 +137,10 @@ void GLWindow::changeRotateMatrix(QMatrix4x4& rotate_matrix, float dx, float dy)
     rotate_matrix.rotate(-dy, 1.0, 0.0, 0.0);
 }
 
-void GLWindow::wheelEvent(QWheelEvent *event)
-{
-    QPoint numDegrees = event->angleDelta() / 8.0;
-    if (!numDegrees.isNull()) {
-        zoffset -= numDegrees.y() * 0.03;
-        resetModelView();
-        update();
-    }
-}
-
 void GLWindow::mousePressEvent(QMouseEvent *event)
 {
     if (event->button() == Qt::LeftButton) {
-        lastMousePosition = event->pos();
+        lastMousePosition = event->position();
         dragging = true;
     }
 }
@@ -90,10 +148,10 @@ void GLWindow::mousePressEvent(QMouseEvent *event)
 void GLWindow::mouseMoveEvent(QMouseEvent *event)
 {
     if ((event->buttons() & Qt::LeftButton) && dragging) {
-        QPoint dp = event->pos() - lastMousePosition;
-        changeRotateMatrix(rotateMatrix, dp.x()*0.5, dp.y()*0.5);
+        QPointF dp = event->position() - lastMousePosition;
+        changeRotateMatrix(rotateMatrix, dp.rx()*0.5, dp.ry()*0.5);
         resetModelView();
-        lastMousePosition = event->pos();
+        lastMousePosition = event->position();
         update();
     }
 }
@@ -101,11 +159,39 @@ void GLWindow::mouseMoveEvent(QMouseEvent *event)
 void GLWindow::mouseReleaseEvent(QMouseEvent *event)
 {
     if (event->button() == Qt::LeftButton && dragging) {
-        QPoint dp = event->pos() - lastMousePosition;
-        changeRotateMatrix(rotateMatrix, dp.x()*0.5, dp.y()*0.5);
+        QPointF dp = event->position() - lastMousePosition;
+        changeRotateMatrix(rotateMatrix, dp.rx()*0.5, dp.ry()*0.5);
         resetModelView();
         dragging = false;
         update();
+    }
+}
+
+void GLWindow::wheelEvent(QWheelEvent *event)
+{
+    QPoint numDegrees = event->angleDelta() / 8.0;
+    if (!numDegrees.isNull()) {
+        zoffset -= numDegrees.y() * (platformL > platformW ? platformL * 0.0005 : platformW * 0.0005);
+        resetModelView();
+        update();
+    }
+}
+
+void GLWindow::keyPressEvent(QKeyEvent *event) {
+    switch (event->key()) {
+        case Qt::Key_R:
+            std::random_device rd;
+            std::mt19937 gen(rd());
+
+            freeAllCells();
+            trees.clear();
+            std::uniform_int_distribution<int> dis(freeCells.size() / 4, freeCells.size() / 2);
+
+            int treeCount = dis(gen);
+            for (int i = 0; i < treeCount; i++)
+                addTree();
+            update();
+            break;
     }
 }
 
@@ -119,6 +205,21 @@ void GLWindow::initShader()
 }
 
 
+GLWindow::GLWindow()
+{
+    platform = std::make_unique<QVector3D[]>(24);
+    getQuadVertices(platform.get(), platformW, platformL, platformH, 0.0, 0.0, 0.0);
+
+    freeAllCells();
+
+
+
+}
+
+GLWindow::~GLWindow()
+{
+}
+
 void GLWindow::initializeGL()
 {
     initializeOpenGLFunctions();
@@ -130,15 +231,6 @@ void GLWindow::initializeGL()
     resetModelView();
 
     initShader();
-
-    platform = std::make_unique<QVector3D[]>(24);
-    getQuadVertices(platform.get(), 100.0, 2.0, 100.0, 0.0, -8.0, 0.0);
-    trees.push_back(std::make_unique<QVector3D[]>(24));
-    getQuadVertices(trees[trees.size() - 1].get(), 10.0, 40.0, 10.0, -30.0, 13.0, -30.0);
-    trees.push_back(std::make_unique<QVector3D[]>(24));
-    getQuadVertices(trees[trees.size() - 1].get(), 10.0, 40.0, 10.0, 30.0, 13.0, -30.0);
-    trees.push_back(std::make_unique<QVector3D[]>(24));
-    getQuadVertices(trees[trees.size() - 1].get(), 10.0, 40.0, 10.0, 0.0, 13.0, 0.0);
 }
 
 void GLWindow::resizeGL(int w, int h)
@@ -161,7 +253,7 @@ void GLWindow::paintGL()
 
 void GLWindow::drawPlatform()
 {
-    quadShaderProgram.setUniformValue("objectColor", QVector3D(50.0/255.0, 75.0/255.0, 40.0/255.0));
+    quadShaderProgram.setUniformValue("objectColor", QVector3D(200.0/255.0, 210.0/255.0, 195.0/255.0));
     quadShaderProgram.setAttributeArray("vertex", platform.get());
     glDrawArrays(GL_QUADS, 0, 6*4);
 }
@@ -169,8 +261,14 @@ void GLWindow::drawPlatform()
 void GLWindow::drawTrees()
 {
     for (const auto &tree : trees) {
-        quadShaderProgram.setUniformValue("objectColor", QVector3D(74.0/255.0, 53.0/255.0, 42.0/255.0));
-        quadShaderProgram.setAttributeArray("vertex", tree.get());
+        quadShaderProgram.setUniformValue("objectColor", tree.getTreeTruncColor());
+        quadShaderProgram.setAttributeArray("vertex", tree.getTreeTrunc());
         glDrawArrays(GL_QUADS, 0, 6*4);
+
+        quadShaderProgram.setUniformValue("objectColor", tree.getTreeLeavesColor());
+        for (auto &leaf : tree.getTreeLeaves()) {
+            quadShaderProgram.setAttributeArray("vertex", leaf.get());
+            glDrawArrays(GL_QUADS, 0, 6*4);
+        }
     }
 }
--- glwindow.h
+++ glwindow.h
@@ -9,13 +9,37 @@
 #include <QWheelEvent>
 #include <QKeyEvent>
 
+class Tree {
+    std::unique_ptr<QVector3D[]> treeTrunc;
+    QVector3D treeTruncColor;
+    std::vector<std::unique_ptr<QVector3D[]>> treeLeaves;
+    QVector3D treeLeavesColor;
+    int type;
+
+public:
+    Tree(int type, float width, float length, float height, float x, float y, float z);
+    QVector3D* getTreeTrunc() const { return treeTrunc.get(); }
+    QVector3D getTreeTruncColor() const { return treeTruncColor; }
+    std::vector<std::unique_ptr<QVector3D[]>> const& getTreeLeaves() const { return treeLeaves; }
+    QVector3D getTreeLeavesColor() const { return treeLeavesColor; }
+};
+
+class House {
+    std::vector<std::unique_ptr<QVector3D[]>> houseWalls;
+    std::vector<std::unique_ptr<QVector3D[]>> houseRoof;
+    int type;
+
+public:
+    House(int type, float width, float length, float height, float x, float y, float z);
+};
+
 class GLWindow : public QOpenGLWindow, protected QOpenGLFunctions
 {
     Q_OBJECT
 
     float zoffset = 3;
 
-    QPoint lastMousePosition;
+    QPointF lastMousePosition;
     bool dragging;
 
     QMatrix4x4 rotateMatrix;
@@ -24,8 +48,17 @@ class GLWindow : public QOpenGLWindow, protected QOpenGLFunctions
 
     QOpenGLShaderProgram quadShaderProgram;
 
+    const float platformW = 1000.0, platformL = 1000.0, platformH = 10.0;
     std::unique_ptr<QVector3D[]> platform;
-    std::vector<std::unique_ptr<QVector3D[]>> trees;
+    const float cellW = 50.0, cellL = 50.0;
+    std::vector<std::pair<int, int>> freeCells;
+
+    std::vector<House> houses;
+    std::vector<Tree> trees;
+
+    void addTree();
+
+    void freeAllCells();
 
     void resetProjection();
     void resetModelView();
@@ -35,6 +68,7 @@ class GLWindow : public QOpenGLWindow, protected QOpenGLFunctions
     virtual void mouseMoveEvent(QMouseEvent *event) override;
     virtual void mouseReleaseEvent(QMouseEvent *event) override;
     virtual void wheelEvent(QWheelEvent *event) override;
+    virtual void keyPressEvent(QKeyEvent *event) override;
 
     void initShader();
 
